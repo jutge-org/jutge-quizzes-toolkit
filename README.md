e8c439676de575e08bfb0b4f55fc4f482909c8f6
Jordi Petit <jordi.petit@gmail.com>
2017-09-04 11:23:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# quizzes-toolkit
-Toolkit to prepare quizzes for Jutge.org
+# Toolkit to prepare quizzes for Jutge.org
+
+## Installation
+
+In order to use the Jutge.org quizzes toolkit,
+please install its dependencies: Python3 and YAML for Python.
+
+In order to install YAML for Python, simply execute
+
+```bash
+pip3 install pyyaml
+```
+
+(You may have to use `pip` rather than `pip3` depending on your Python installation).
+
+
+## Sample quizzes
+
+Some sample quizzes are given under the `quizzes` directory.
+
+
+## How to execute
+
+In order to generate a quiz, simply execute `make-quiz.py` inside a directory
+that contains the quiz, passing as a unique parameter an integer number that
+will be used as the random seed. The output will be a JSON file with the generated
+quiz.
+
+
+## Documentation
+
+TBD !!!
+
+
---
+++ bin/make-quiz.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+# !!! Falta fer shuffle d'alguns items, ara només ho fa dels choices
+
+
+import sys, string, pprint, yaml, json, random, os, time
+
+def build_q(fname):
+
+    py_name = fname + ".py"
+    yml_name = fname + ".yml"
+
+    # read the python code
+    if os.path.exists(py_name):
+        code = open(py_name).read()
+    else:
+        code = ""
+
+    # read the question description
+    q = open(yml_name).read()
+
+    # execute the code, using new global and local dictionaries
+    ldict = {}
+    exec(code, globals(), ldict)
+
+    # modify the question description with the local dictionary
+    subs = string.Template(q).substitute(ldict)
+
+    # get the text back to data
+    output = yaml.load(subs)
+
+    # fix fields according to type
+    if output["type"] in ["SingleChoice", "MultipleChoice"]:
+        # shuffle choices if needed
+        if 'choices' in output and output.get('shuffle', True):
+            random.shuffle(output['choices'])
+    elif output["type"] == "FillIn":
+        # shuffle options for all items
+        for item in output["items"].values():
+            if 'options' in item and item.get('shuffle', True):
+                random.shuffle(item['options'])
+
+    # return the output
+    return output
+
+
+def main():
+    seed = int(sys.argv[1])
+    quiz = yaml.load(open("quiz.yml"))
+
+    random.seed(seed)
+
+    if quiz.get('shuffle', True):
+        random.shuffle(quiz['questions'])
+
+    quiz['seed'] = seed
+    quiz['time-generation'] = time.ctime()  # !!! posar format YYYY-MM-DD HH:MM:SS
+
+    for question in quiz['questions']:
+        random.seed(seed)
+        question['q'] = build_q(question['file'])
+        question['a'] = {}
+        question['points'] = 0
+
+    json.dump(quiz, sys.stdout, indent=4)
+
+main()
