04aa68f9dec60d086784c83a7bece4baf92324dc
Ralusama19 <ralucris.v@gmail.com>
2017-09-18 21:42:33 +0200

    checked types in make quiz

--- bin/make-quiz.py
+++ bin/make-quiz.py
@@ -16,18 +16,29 @@ def build_sc(output, title):
         error(title,'A list of Choices must be defined!')
     #make sure each choice has text
     correct = False
+    false_answers = []
     for choice in output["choices"]:
-        if not choice.get("text", False):
+        text = choice.get("text", False)
+        if not text:
             error(title,'Text must be provided for every choice!')
+
+        #check types
+        check_not_dict_list(text, title, "Text", "basic type")
+        check_not_dict_list(choice.get("hint"), title, "Hint", "basic type")
         #check that at most one correct answer is provided
         if choice.get("correct", False):
             if correct:
                 error(title,'Only one correct answer must be provided!')
             correct = True
+            correct_text = choice.get("text")
+        else:
+            false_answers.append(text)
     #check that at least one correct answer is provided
     if not correct:
         error(title,'A correct answer must be provided!')
-
+    #check that correct answer is not repeated
+    if correct_text in false_answers:
+        error(title, 'Only one correct answer with the same text must be provided!')
     #shuffle the choices if needed
     if (output.get('shuffle', True)):
         random.shuffle(output['choices'])
@@ -44,14 +55,13 @@ def build_mc(output, title):
     #make sure each choice has text
     correct = False
     for choice in output["choices"]:
-        if not choice.get("text", False):
+        text = choice.get("text", False)
+        if not text:
             error(title,'Text must be provided for every choice!')
-        #check that at least one correct answer is provided
-        if choice.get("correct", False):
-            correct = True
-    if not correct:
-        error(title,'At least one correct answer must be provided!')
+        check_not_dict_list(text, title, "Text", "basic type")
+        check_not_dict_list(choice.get("hint"), title, "Hint", "basic type")
 
+    
     #shuffle the choices if needed
     if (output.get('shuffle', True)):
         random.shuffle(output['choices'])
@@ -62,63 +72,107 @@ def build_mc(output, title):
 # function to build Ordering questions
 def build_o(output, title):
     #check non empty label
-    if output.get("label") == None:
+    label = output.get("label")
+    if label == None:
         error(title,'A label for the choice list must be provided!')
+    check_not_dict_list(label, title, "label", "text")
     #check non empty items
     items = output.get("items", False)
     if not items:
         error(title,'A list of items must be provided!')
+    check_list(items, title, "items")
+    for item in items:
+        check_not_dict_list(item, title, "Items", "text")
+    output["items_rand"] = list(items)
+    random.shuffle(output["items_rand"])
     return output
-
-def check_writable_item(item, title):
-    #check for mandatory fields
-    if item.get("maxlength") == None:
-        error(title,'Invalid item! Did you forget the list of options or maxlength?')
-    if item.get("correct") == None:
-        error(title,'All correct answers must be provided!')
-    item.update({'ignorecase' : item.get("ignorecase", True), 'trim' : item.get("trim", True), 'placeholder' : item.get("placeholder", '?')})
-    # ??? placeholder es opcional ??? TBD
-
-def check_dropdown_item(item, title):
-    if item.get("correct") == None:
-        error(title,'All correct answers must be provided!')
-    if item.get("options") == None:
-        error(title,'A list of options must be provided!')
-    #check that the correct option is available
-    correct = False
-    for option in item.get("options"):
-        if option == item.get("correct"):
-            correct = True
-    # !!! potser podriem cambiar la manera de llegir opcions per no haver d'escriure la correcta a la llista tambe
-    if not correct:
-        error(title, "All correct answers must be in the options!")
 
 # function to build FillIn questions
 def build_fi(output, title):
     #check non empty context
-    if not output.get("context", False):
+    context = output.get("context", False)
+    if not context:
         error(title,'A context must be provided!')
+    check_not_dict_list(context, title, "Context", "text")
     #check non empty items
     if not output.get("items", False):
         error(title,'An item list must be provided!')
-    for item in output["items"]:
-        if not output["items"].get(item).get("options", False):
-            check_writable_item(output["items"].get(item), title)
+    for name, item in output["items"].iteritems():
+        if not name in context:
+            error(title, "Item "+name+" is not in the context!")
+        if not item.get("options", False):
+            check_writable_item(item, title)
         else:
-            check_dropdown_item(output["items"].get(item), title)
+            check_dropdown_item(item, title)
     return output
+
+def check_writable_item(item, title):
+    #check for mandatory fields
+    maxlength = item.get("maxlength") 
+    if maxlength == None:
+        error(title,'Invalid item! Did you forget the list of options or maxlength?')
+    if not isinstance(maxlength, int):
+        error(title, "Maxlength must be an integer!")
+
+    correct = item.get("correct")
+    if correct == None:
+        error(title,'All correct answers must be provided!')
+    check_not_dict_list(correct, title, "Correct", "basic type")
+
+    item.update({'ignorecase' : item.get("ignorecase", True), 'trim' : item.get("trim", True), 'placeholder' : item.get("placeholder", '?')})
+    # ??? placeholder es opcional ??? TBD
+
+    check_not_dict_list(item.get("ignorecase"), title, "ignorecase", "true or false")
+    check_not_dict_list(item.get("trim"), title, "trim", "true or false")
+    check_not_dict_list(item.get("placeholder"), title, "placeholder", "a text")
+
+def check_dropdown_item(item, title):
+    correct = item.get("correct")
+    if correct == None:
+        error(title,'All correct answers must be provided!')
+    check_not_dict_list(correct, title, "Correct", "basic type")
+
+    options = item.get("options")
+    if options == None:
+        error(title,'A list of options must be provided!')
+    check_list(options, title, "options")
+    #check that the correct option is available
+    correct = False
+    false_answers = []
+    for option in options:
+        if option == item.get("correct"):
+            correct = True
+        check_not_dict_list(option, title, "All options", "basic type")
+    # !!! potser podriem cambiar la manera de llegir opcions per no haver d'escriure la correcta a la llista tambe
+    if not correct:
+        error(title, "The correct answer must be in the options!")
 
 
 # function to build Matching questions
 def build_m(output, title):
-    #check for labels, left and right
-    if not output.get("labels", False) or len(output["labels"]) != 2:
+    #check for labels, left and 
+    labels = output.get("labels", False)
+    check_list(labels, title, "labels")
+    if not labels or len(labels) != 2:
         error(title, "A list with two labels must be provided!")
-    if not output.get("left", False) or not output.get("right", False):
+    left = output.get("left", False)
+    right = output.get("right", False)
+    if not left or not right:
         error(title, "Two lists (right and left) must be provided!")
+    check_list(left, title, "left")
+    check_list(right, title, "right")
     #make sure left and right have the same size
-    if len(output.get("right")) != len(output.get("left")):
+    if len(right) != len(left):
         error(title,"The right and left lists must have the same size")
+    for item in left:
+        check_not_dict_list(item, title, "left list's items", "basic type")
+    for item in right:
+        check_not_dict_list(item, title, "right list's items", "basic type")
+    output["right_rand"] = list(right)
+    output["left_rand"] = list(left)
+    random.shuffle(output["right_rand"])
+    random.shuffle(output["left_rand"])
+
     return output
 
 
@@ -152,10 +206,16 @@ def build_q(fname, title):
     output = yaml.load(subs)
 
     #make sure we have the mandatory attributes
-    if output.get("text") == None:
+    text = output.get("text")
+    if text == None:
         error(title, "Missing text!")
-    if output.get("type") == None:
+    check_not_dict_list(text, title, "Text", "string")
+
+    qtype = output.get("type")
+    if qtype == None:
         error(title, "Missing type!")
+    check_not_dict_list(text, title, "Question type", "just a word")
+
 
     # fix fields according to type
     if output["type"] == "SingleChoice":
@@ -173,6 +233,14 @@ def build_q(fname, title):
     else:
        error(title, "Incorrect question type!")
 
+#so many arguments :(
+def check_not_dict_list(thing,title,name,goal):
+    if isinstance(thing, list) or isinstance(thing,dict):
+        error(title, name+" must be a "+goal+"!")
+
+def check_list(thing,title,name):
+    if not isinstance(thing, list):
+        error(title, name+" must be a list!")
 
 def main():
     seed = int(sys.argv[1])
@@ -186,10 +254,17 @@ def main():
     quiz['seed'] = seed
     quiz['time-generation'] = time.ctime()  # !!! posar format YYYY-MM-DD HH:MM:SS
 
+    #Check types
+    check_not_dict_list(quiz.get("statement"), "Quiz", "Statement", "text")
+    check_not_dict_list(quiz.get("title"), "Quiz", "Title", "text")
+
     score_sum = 0
     for question in quiz['questions']:
         random.seed(seed)
-        score_sum += question.get("score", 0)
+        score = question.get("score", 0)
+        if not isinstance(score,int):
+            error("quiz", "All scores must be integers")
+        score_sum += score
         if not question.get("file", False) or not question.get("title", False):
             error("quiz","All questions need a file and a title!")
         question['q'] = build_q(question['file'], question['title'])
